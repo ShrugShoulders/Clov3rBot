e9d5bb94a2aa410043cbebc49d1f1780e3dd3d2d
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-01-27 16:48:13 -0700

    Update Clov3r_async.py

--- Clov3r_Async/Clov3r_async.py
+++ Clov3r_Async/Clov3r_async.py
@@ -327,7 +327,7 @@ class IRCBot:
 
     async def extract_webpage_title(self, url):
         try:
-            response = requests.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers, timeout=(1.0, 0.5))
             response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
 
             # Check if the content type is an image or plain text
@@ -572,6 +572,7 @@ class IRCBot:
             ".stats",
             ".version",
             ".sed",
+            ".weather",
             ".admin",
         ]
         if exclude_admin:
@@ -596,6 +597,7 @@ class IRCBot:
             ".stats": "Stats command: Display statistics for a user. Use '.stats <user>'.",
             ".version": "Version command: Shows the version of Clov3r",
             ".sed": "Sed usage s/change_this/to_this/(g/i). Flags are optional. To include word boundaries use \\b Example: s/\\btest\\b/stuff. I can also take regex.",
+            ".weather": "Search weather forecast - example: .weather Ireland - Can search by address or other terms"
             ".admin": ".factadd - .quit - .join - .part - .op - .deop - .botop - .reload - .purge",
         }
 
@@ -668,6 +670,9 @@ class IRCBot:
                             # PNOG
                             response = f"PRIVMSG {channel} :{sender}: PNOG!"
                             await self.send(response)
+
+                        case '.weather':
+                            await self.get_weather(args, channel)
 
                         case '.roll':
                             # Roll the dice
@@ -776,6 +781,103 @@ class IRCBot:
 
                         case '.purge' if hostmask in self.admin_list:
                             await self.purge_message_queue(channel, sender)
+
+    async def geocode_location(self, location):
+        # If the location is empty, return None
+        if not location:
+            return None, None
+
+        try:
+            # Make a request to retrieve the latitude and longitude for the location
+            response = requests.get(f"https://geocode.maps.co/search?q={location}&api_key=65b583605ab6a403481192yza5a9247")
+            print("Geocoding response status code:", response.status_code)
+            print("Geocoding response content:", response.content)
+            
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Parse the JSON response
+                data = response.json()
+                print("Geocoding response data:", data)
+                
+                # Extract latitude and longitude from the first place_id
+                if data:
+                    first_place = data[0]
+                    latitude = round(float(first_place["lat"]), 4)
+                    longitude = round(float(first_place["lon"]), 4)
+                    return latitude, longitude
+
+            # If unable to get latitude and longitude from the location
+            print("Unable to geocode the location:", location)
+            return None, None
+            
+        except Exception as e:
+            print("An error occurred while geocoding:", e)
+            return None, None
+
+    async def get_weather(self, location, channel):
+        # Set your user agent
+        user_agent = "Clov3r_forecast, connorkim.kim3@gmail.com"
+
+        # Get latitude and longitude from geocoding
+        lat, lon = await self.geocode_location(location)
+
+        # If unable to geocode the location, respond accordingly
+        if lat is None or lon is None:
+            response = f"PRIVMSG {channel} :Unable to get latitude and longitude for the location: {location}."
+            await self.send(response)
+            return
+
+        # Get the forecast data for the given latitude and longitude
+        try:
+            # Make a request to retrieve the weather forecast data
+            response = requests.get(f"https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={lat}&lon={lon}", headers={"User-Agent": user_agent})
+            print("Response status code:", response.status_code)
+            print("Response content:", response.content)
+            
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Parse the JSON response
+                data = response.json()
+                
+                # Extract relevant weather information
+                timeseries = data.get("properties", {}).get("timeseries", [])
+                
+                if timeseries:
+                    # Get the current forecast (first entry in timeseries)
+                    current_forecast = timeseries[0]
+                    print("Current forecast:", current_forecast)
+                    
+                    # Extract data from the current forecast
+                    instant_details = current_forecast.get("data", {}).get("instant", {}).get("details", {})
+                    print("Instant details:", instant_details)
+                    next_1_hours_summary = current_forecast.get("data", {}).get("next_1_hours", {}).get("summary", {})
+                    print("Next 1 hour summary:", next_1_hours_summary)
+                    next_6_hours_summary = current_forecast.get("data", {}).get("next_6_hours", {}).get("summary", {})
+                    print("Next 6 hours summary:", next_6_hours_summary)
+                    
+                    # Construct weather forecast message
+                    forecast_message = f"{location}, lat={lat}, lon={lon}:"
+                    temp_message = f"Current temperature: {instant_details.get('air_temperature')} °C"
+                    cloud_message = f"Cloud coverage: {instant_details.get('cloud_area_fraction')}%"
+                    humidity_message = f"Humidity: {instant_details.get('relative_humidity')}%"
+                    wind_direction = f"Wind Direction: {instant_details.get('wind_from_direction')}"
+                    wind_speed = f"Wind Speed: {instant_details.get('wind_speed')}"
+                    nxt1hr_message = f"Next 1 hour: {next_1_hours_summary.get('symbol_code', 'N/A')}"
+                    nxt6hr_message = f"Next 6 hours: {next_6_hours_summary.get('symbol_code', 'N/A')}"
+                    
+                    # Send weather forecast to the channel
+                    response = f"PRIVMSG {channel} :{forecast_message} " + f"{temp_message} " + f"{cloud_message} " + f"{humidity_message} " + f"{wind_speed} " + f"{wind_direction}" + f"{nxt1hr_message} " + f"{nxt6hr_message} "
+                    await self.send(response)
+                    return
+                
+            # If no forecast available
+            response = f"PRIVMSG {channel} :No forecast available for location: {location}."
+            await self.send(response)
+            
+        except Exception as e:
+            print("An error occurred:", e)
+            response = f"PRIVMSG {channel} :An error occurred while fetching weather information."
+            await self.send(response)
 
     async def stats_command(self, channel, sender, content):
         # Extract the target user from the command
@@ -1078,14 +1180,11 @@ class IRCBot:
                 old = old.replace("__SLASH__", "/")
                 new = new.replace("__SLASH__", "/")
 
-                # Escape newline characters in the new string
-                new = new.replace("\n", " ").replace("\r", " ")
-
-                # Escape control characters in the old string
-                old = re.escape(old)
-
                 # Check for word boundaries flag
                 word_boundaries = r'\b' if '\\b' in old else ''
+
+                # If the old string contains \d, replace it with [0-9]
+                old = old.replace(r'\\d', r'[0-9]')
 
                 # Update the regular expression with word boundaries
                 regex_pattern = fr'{word_boundaries}{old}{word_boundaries}'
@@ -1110,8 +1209,8 @@ class IRCBot:
                     # Set count based on the global flag
                     count = 0 if 'g' in flags else 1
 
-                    # Extract color codes from the original message
-                    color_codes = re.findall(r'\x03\d{0,2}(?:,\d{1,2})?', original_message)
+                    # Extract color codes from the original message using the find_color_codes function
+                    color_codes = self.find_color_codes(original_message)
 
                     # Replace old with new using regex substitution
                     replaced_message = re.sub(regex_pattern, new, original_message, flags=regex_flags, count=count)
@@ -1173,6 +1272,23 @@ class IRCBot:
             response = f"PRIVMSG {channel} :[\x0304Sed\x03] Invalid sed command format\r\n"
             await self.send(response)
             print(f"Sent: {response} to {channel}")
+
+    def find_color_codes(self, original_message):
+        color_codes = []
+        i = 0
+        while i < len(original_message):
+            if original_message[i] == '\x03':
+                # Check if the following characters form a valid color code pattern
+                code = original_message[i:i+3]
+                if code[1:].isdigit() or (code[1] == ',' and code[2:].isdigit()):
+                    color_codes.append(code)
+                    i += 3
+                else:
+                    i += 1
+            else:
+                i += 1
+
+        return color_codes
 
     async def disconnect(self):
         if self.writer:
