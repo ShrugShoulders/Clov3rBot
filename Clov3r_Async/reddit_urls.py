6273fb69e421b30844b552dd76080b39b22241d5
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-05-05 20:52:30 -0400

    Add files via upload

---
+++ Clov3r_Async/reddit_urls.py
@@ -0,0 +1,43 @@
+import asyncio
+import requests
+import re
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+
+async def process_reddit_url(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0', 'Accept-Encoding': 'identity'}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            title_tag = soup.find('title')
+            if title_tag:
+                return f"[\x0303Reddit\x03] {title_tag.text}"
+            else:
+                return "Title not found"
+        else:
+            print(f"Error: Response status {response.status_code}")
+            return
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+async def parse_reddit_url(normalized_content):
+    url_regex = re.compile(r'https?://[^\s\x00-\x1F\x7F]+')
+    match = url_regex.search(normalized_content)
+    if match:
+        url = match.group()
+        parsed_url = urlparse(url)
+        hostname = parsed_url.hostname
+
+        if hostname in ['reddit.com', 'www.reddit.com']:
+            old_reddit_url = url.replace(hostname, 'old.reddit.com')
+            return await process_reddit_url(old_reddit_url)
+        elif hostname in ['www.old.reddit.com', 'old.reddit.com']:
+            return await process_reddit_url(hostname)
+        else:
+            return
+            print(f"URL: {url} is not a reddit url")
+    else:
+        pass
